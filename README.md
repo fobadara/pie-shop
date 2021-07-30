a9940d2d55e203ff99f0443e0f4177e38c58b82e
Abdulfatai <fobadara@gmail.com>
2021-07-30 11:34:45 +0100

    Finish building pie shop

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # pie-shop
+
+A site for selling pies
+
+![screenshot](images/homepage.png)
+
+Built With
+
+● HTML
+
+● CSS
+
+● Javascript
+
+# Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+⬜ clone the app by running this command `git clone https://github.com/fobadara/pie-shop.git`
+
+⬜ Run this command `cd my-portfolio-site-1`
+
+⬜ Open in browser
+
+# Prerequisites
+
+You will be needing:
+
+⬜ A web browser to view output
+
+⬜ A text editor or terminal
+
+# Run tests
+
+You can run tests by following these steps:
+
+⬜ commit and push your changes
+
+⬜ navigate to pull request
+
+⬜ I have created a pull request which runs linters or you could simply create a pull request of your own
+
+# Authors
+
+👤 Abdulfatai Badara
+
+GitHub: github.com/fobadara
+
+Twitter: fob90s@twitter.com
+
+# Contributing 🤝
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page.
+
+# Show your support
+
+Give a ⭐️ if you like this project!
+
+# Acknowledgments
+
+Hat tip to anyone whose code was used
+
+A big thank you to pngtree whose images have been used:
+
+⬜ purple PNG Designed By CHENXIN from https://pngtree.com/freepng/purple-bakery-clip-art_5894548.html?sol=downref&id=bef
+
+⬜ features PNG Designed By CHENXIN from https://pngtree.com/freepng/featured-orange-bakery-clip-art_5894553.html?sol=downref&id=bef
+
+Lots of appreciations to my wife, Habeebah who keeps supporting me
+
+# License 📝
+
+This project is licensed by [MIT](LICENSE)
---
+++ script/script.js
@@ -0,0 +1,70 @@
+window.addEventListener("DOMContentLoaded", function (e) {
+  
+  // Script for pie.html
+  const orderButtons = document.querySelectorAll("button[data-order]");
+  orderButtons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      const button = e.currentTarget;
+      const container = button.parentNode;
+
+      const order = {
+        id: (id = button.getAttribute("data-order")),
+        title: container.querySelector(".title").innerText,
+        price: container.querySelector(".price").innerText,
+        desc: container.querySelector(".desc").innerText,
+      };
+
+      localStorage.setItem("order", JSON.stringify(order));
+
+      const url = window.location.href.replace("pies.html", "order.html");
+      window.location.href = url;
+    });
+  });
+
+
+  let locationBox = document.querySelector("#location");
+  let location = {
+    latitude: "unknown",
+    longitude: "unknown",
+  };
+
+  window.navigator.geolocation.getCurrentPosition(
+    function (position) {
+      location = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+
+      locationBox.value = JSON.stringify(location);
+    },
+    function (error) {
+      if (locationBox != null) {
+        locationBox.value = JSON.stringify(location);
+      }
+    }
+  );
+
+  //Script for order.html
+  const order = localStorage.getItem("order");
+  const pieOrder = JSON.parse(order);
+
+  if (order) {
+    const pie = document.querySelector(".pie");
+    const title = pie.querySelector(".title");
+    const price = pie.querySelector(".price");
+    const desc = pie.querySelector(".desc");
+    const orderInput = document.querySelector("#pie-order");
+
+    title.innerText = pieOrder.title;
+    price.innerText = pieOrder.price;
+    desc.innerText = pieOrder.desc;
+
+    if (orderInput != null) {
+      orderInput.value = order;
+    }
+      
+    const img = pie.querySelector("img");
+    img.setAttribute("src", `images/${pieOrder.id}.png`);
+    img.setAttribute("alt", pieOrder.title);
+  }
+});
